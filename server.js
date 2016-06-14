33766b78d5182fbae856380388bfb7308429cbb8
Justin Hu <Justin Hu@FSIINC.NET>
2016-06-14 18:12:05 -0400

    Temporily working on a web portal

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/web_client/home.html')
+	res.sendFile(__dirname + '/web_client/views/home.html')
 })
 
-app.use('/node_modules', express.static(__dirname + '/node_modules'))
+// app.use('/node_modules', express.static(__dirname + '/node_modules'))
+app.use('/js', express.static(__dirname + '/web_client/js'));
+app.use('/css', express.static(__dirname + '/web_client/css'));
+
 
 app.listen(3000, function(){
 	console.log("This is a test from Edward!")
