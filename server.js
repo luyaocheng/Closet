e6899c613e06d89cc8e43101126f41e176e542ff
Edward Hu <jhu886688@gmail.com>
2016-06-14 08:41:54 -0400

    I added web UI for testing first

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/testIndex.html')
+	res.sendFile(__dirname + '/web_client/home.html')
 })
+
+app.use('/node_modules', express.static(__dirname + '/node_modules'))
 
 app.listen(3000, function(){
 	console.log("This is a test from Edward!")
