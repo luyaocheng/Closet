bc1d0d475c5488cc46e968c4bab55562f4f758e2
Edward Hu <jhu886688@gmail.com>
2016-06-13 21:46:29 -0400

    Adding Express envir and testIndex.html for further testing

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("This is a test from Edward!")
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/testIndex.html')
+})
+
+app.listen(3000, function(){
+	console.log("This is a test from Edward!")
+})
+
+
